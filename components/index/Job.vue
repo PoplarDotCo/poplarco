625be4cf8c6f304cec84558ed0346958ee978121
Mari <mari@beesgo.co>
2022-04-04 13:21:00 -0400

    job cards

--- components/index/Job.vue
+++ components/index/Job.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="job">
-    <h5>{{ jobTitle }}</h5>
+    <h5>{{ title }}</h5>
+    <p>
+      <span>{{ type }}</span>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<span>{{ location }}</span>
+    </p>
+
+    <div class="desc">
+      <p>{{ description }}</p>
+    </div>
+
+    <div class="links">
+      <h6>Learn More:</h6>
+
+      <div class="row direction_row">
+        <a :href="linkedIn" class="button button_sm" target="_blank" v-if="linkedIn">LinkedIn</a>
+        <a :href="angelList" class="button button_sm" target="_blank" v-if="angelList">Angel List</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,8 +30,23 @@ export default {
     }
   },
   computed: {
-    jobTitle() {
-      return this.job && this.job && this.job.job_title;
+    title() {
+      return this.job && this.job.job_title;
+    },
+    type() {
+      return this.job && this.job.job_type;
+    },
+    location() {
+      return this.job && this.job.job_location;
+    },
+    description() {
+      return this.job && this.job.job_description;
+    },
+    linkedIn() {
+      return this.job && this.job.job_linkedin_link && this.job.job_linkedin_link.url;
+    },
+    angelList() {
+      return this.job && this.job.job_angel_list_link && this.job.job_angel_list_link.url;
     }
   }
 }
@@ -27,5 +58,18 @@ export default {
   border-radius: $borderRadius;
   padding: $cardPadding;
   margin: 30px 0;
+
+  h5 {
+    margin-bottom: 0;
+  }
+
+  h6 {
+    margin-top: 30px;
+  }
+
+  a {
+    margin: 20px 30px 0 0;
+  }
+
 }
 </style>
