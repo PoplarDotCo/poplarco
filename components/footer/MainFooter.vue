287bf42bb79313c13d5c2c2a43ff2c8539ec4cff
Mari <mari@beesgo.co>
2022-04-06 00:59:36 -0400

    footer init

--- components/footer/MainFooter.vue
+++ components/footer/MainFooter.vue
@@ -1,17 +1,68 @@
 <template>
   <section id="footer">
-
+    <div class="container">
+      <div class="row direction_row">
+        <div class="left">
+          <img src="~/assets/img/logo_black.png" alt="Poplar Logo">
+          <p>&copy; {{ getYear }} Poplar Labs, Inc.</p>
+        </div>
+        <div class="right">
+          <p class="bold">Get In Touch</p>
+          <p><a href="#contact-us">Contact Us</a></p>
+          <p>(201) 801 - 7789</p>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'MainFooter'
+  name: 'MainFooter',
+  props: {},
+  method: {
+    getYear () {
+      console.log('test');
+      // const date = new Date();
+      // console.log(date.getFullYear());
+      // return date.getFullYear();
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 #footer {
   background: $colorMidGray;
+  margin-top: 60px;
+
+  .row {
+    justify-content: space-between;
+  }
+
+  .left {
+    img {
+      width: 150px;
+    }
+    p {
+      font-weight: $fontWeightMedium;
+      margin-left: 8px;
+    }
+  }
+
+  .right {
+
+    .bold {
+      font-weight: $fontWeightBold;
+    }
+
+    a,
+    a:visited,
+    a:focus,
+    a:active {
+      color: $black;
+      text-decoration: underline;
+    }
+  }
 }
 </style>
