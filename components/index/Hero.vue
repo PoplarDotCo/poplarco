922f586c8b96426fa54501c120a9e9945855a224
Mari <mari@beesgo.co>
2022-04-07 22:41:49 -0400

    hero image fix desktop

--- components/index/Hero.vue
+++ components/index/Hero.vue
@@ -2,10 +2,7 @@
   <section id="hero">
     <div class="container">
       <div class="row direction_row">
-        <div class="left-hero">
-          <div class="image"></div>
-          <img src="~/assets/img/kids-in-trash.png" alt="">
-        </div>
+        <div class="left-hero"></div>
         <div class="right-hero">
           <div class="content">
             <h1>What are we <span class="desktop-breakpoint"></span>leaving behind?</h1>
@@ -27,46 +24,31 @@ export default {
 <style lang="scss">
 #hero {
 
+  .row {
+
+
+  }
+
   .left-hero {
     width: 40%;
-
-    img {
-      max-width: 600px;
-      width: 100%;
-      border-radius: $borderRadius;
-    }
+    background: #fff url('~assets/img/kids-in-trash.png') no-repeat top center;
+    background-size: cover;
+    border-radius: $borderRadius;
 
   }
 
   .right-hero {
     width: 60%;
-    //height: 100%;
-    padding-left: 50px;
-    //@include flex(column, nowrap, center, center);
-
-    //h4 {
-    //  margin-bottom: 40px;
-    //}
+    padding: 50px;
   }
 
-  //@include breakpoint(xs-down) {
-  //  height: calc(100vh - 100px);
-  //
-  //  .row {
-  //    @include flex(column);
-  //
-  //    .left-hero {
-  //      height: 100px;
-  //      width: 75%;
-  //    }
-  //
-  //    .right-hero {
-  //      padding: 30px 0;
-  //      justify-content: flex-start;
-  //    }
-  //
-  //  }
-  //
-  //}
+  @include breakpoint(xxs-down) {
+
+    .direction_row {
+      flex-direction: column;
+    }
+
+  }
+
 }
 </style>
